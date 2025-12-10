0be2cf0091336d3b5c9f13c340f12bab898d250c
Piyush Eklavya <70015666+piyusheklavya777@users.noreply.github.com>
2025-12-10 18:35:46 +0530

    fix: update checksum for v1.0.0 release

--- karpi.rb
+++ karpi.rb
@@ -2,7 +2,7 @@ class Karpi < Formula
   desc "Developer Productivity CLI - Unleash Your Potential"
   homepage "https://github.com/piyusheklavya777/karpi"
   url "https://github.com/piyusheklavya777/karpi/releases/download/v1.0.0/karpi-v1.0.0.tar.gz"
-  sha256 "aaf4f03f6e45d56d5f6ff9dba6f049178b2f769f3b0e7f74180cf895b0cb8cea"
+  sha256 "7e005cb47e8285e9255240dda6ef27229ed407667159a14110afe4ceff6aca31"
   version "1.0.0"
 
   def install
