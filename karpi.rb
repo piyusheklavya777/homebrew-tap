b314136c9b6156a5268bc517e614f07e213366b8
Piyush Eklavya <70015666+piyusheklavya777@users.noreply.github.com>
2025-12-10 18:51:29 +0530

    chore: bump karpi to v1.0.1

--- karpi.rb
+++ karpi.rb
@@ -1,9 +1,9 @@
 class Karpi < Formula
   desc "Developer Productivity CLI - Unleash Your Potential"
   homepage "https://github.com/piyusheklavya777/karpi"
-  url "https://github.com/piyusheklavya777/karpi/releases/download/v1.0.0/karpi-v1.0.0.tar.gz"
-  sha256 "7e005cb47e8285e9255240dda6ef27229ed407667159a14110afe4ceff6aca31"
-  version "1.0.0"
+  url "https://github.com/piyusheklavya777/karpi/releases/download/v1.0.1/karpi-v1.0.1.tar.gz"
+  sha256 "2a0d49066af415d97fb95143060fd98661c823c3d683f461ac6d98e6d63dc335"
+  version "1.0.1"
 
   def install
     bin.install "karpi"
